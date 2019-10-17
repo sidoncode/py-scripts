1850a0d49a1327a87c4bf3d975e5f603905da4aa
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2019-10-17 20:24:13 +0530

    first push

--- app1.py
+++ app1.py
@@ -1,14 +1,22 @@
-import os
-import sys 
-#import time
-import bs4
-#import beautifulsoup 4 
-import request
-#from bs4 import beautifulsoup4
-
-def main():
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 
+def G_search():
+    browser = webdriver.Chrome(executable_path="C:\Drivers\chromedriver_win32/chromedriver.exe")
+    browser.get('http://www.google.com')
 
+    search = browser.find_element_by_name('q')
+    search.send_keys("google search through python")
+    search.send_keys(Keys.RETURN) # hit return after you enter search text
+    time.sleep(5) #sleep for 5 sec after seeing the result
+    browser.quit()
+
+
+G_search()
 
 
