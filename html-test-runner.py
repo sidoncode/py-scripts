44478874ae0fa0eb7ac4a6a0f3affebd5b6b3a96
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2019-10-17 23:32:24 +0530

    html test runner

---
+++ html-test-runner.py
@@ -0,0 +1,37 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.webdriver import *
+import unittest
+import HtmlTestRunner
+
+
+class LoginTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path="C:\Drivers\chromedriver_win32/chromedriver.exe")
+        cls.driver.implicitly_wait(10)
+        cls.driver.maximize_window()
+
+    def test_login_valid(self):
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        self.driver.find_element_by_id("txtUsername").send_keys("Admin")
+        self.driver.find_element_by_id("txtPassword").send_keys("admin123")
+        self.driver.find_element_by_id("btnLogin").click()
+        self.driver.find_element_by_id("welcome").click()
+        self.driver.find_element_by_link_text("Logout").click()
+        time.sleep(4)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.driver.close()
+        cls.driver.quit()
+    if __name__ == '__main__':
+        unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output="C:/Users/Sidd/PycharmProjects/selenium_project/reports/reports.html"))
+
+
+
+
